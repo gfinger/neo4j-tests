398225be8743f3cbf75d7c54e4fd70a72f82fde2
d022051 <gregor.karl.frey@sap.com>
2015-06-14 17:01:19 +0200

    @Fetch on Set<Leaf>

--- neo-spring-test/src/main/java/composite/Composite.java
+++ neo-spring-test/src/main/java/composite/Composite.java
@@ -2,7 +2,10 @@ package composite;
 
 import java.util.Set;
 
+import org.springframework.data.neo4j.annotation.Fetch;
+
 public class Composite extends Component {
+	@Fetch
 	private Set<Leaf> leaf;
 
 	public Set<Leaf> getLeaf() {
